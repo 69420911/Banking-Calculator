625de7b5d91749c620c0c886bd95ff0b7b713985
Gordon Vaynapel <gordon.vaynapel@salesforce.com>
2017-11-16 11:23:34 -0800

    Initial Commit

---
+++ Banking Calculator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Banking Calculator
+//
+//  Created by Gordon Vaynapel on 11/16/17.
+//  Copyright © 2017 Gordon Vaynapel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
