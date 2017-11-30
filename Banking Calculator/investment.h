bbde161d2e65539cfa99bbdcd20da33e42b0781c
Gordon Vaynapel <gordon.vaynapel@salesforce.com>
2017-11-30 02:22:24 -0800

    Cleanup/Added a few items

--- Banking Calculator/investment.h
+++ Banking Calculator/investment.h
@@ -2,28 +2,32 @@
 
 using namespace std;
 
-
-class Investment{
-	public:
-		void SetTime(int);
-		void SetAmount(double);
-        void SetInterestRate(double);
-        void SetNumComp (int);
-        void SetSimpleInteret();
-        void SetCompoundedInterest();
-        void CalcSimpleInt();
-        void CalcCompoundedInt();
-        void DisplaySimpleInt();
-        void DisplayCompoundedInt();
-		int GetTime();
-		double GetAmount();
-		double GetInterestRate();
-		int GetNumComp();
-	private:
-		int timeMonthly;
-		double initialAmount;
-		double intRate;
-        double SimpleIntrest;
-        double CompoundIntrest;
-		int numComp;
+class Investment {
+public:
+    void SetTime(int);
+    void SetAmount(double);
+    void SetInterestRate(double);
+    void SetNumComp (int);
+    void SetSimpleInteret();
+    void SetCompoundedInterest();
+    void CalcSimpleInt();
+    void CalcCompoundedInt();
+    void DisplaySimpleInt();
+    void DisplayCompoundedInt();
+    
+    double GetAmount();
+    double GetInterestRate();
+    double GetSimpleIntrest();
+    double GetCompoundedInterest();
+    
+    int GetNumComp();
+    int GetTime();
+private:
+    double initialAmount;
+    double intRate;
+    double SimpleInterest;
+    double CompoundInterest;
+    int numComp;
+    int timeMonthly;
+    
 };
