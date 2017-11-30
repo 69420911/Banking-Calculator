8e6bbc43c452c3725cf28b96c290758fbf6438fd
Gordon Vaynapel <gordon.vaynapel@salesforce.com>
2017-11-30 02:22:12 -0800

    Cleaned it up

--- Banking Calculator/investment.cpp
+++ Banking Calculator/investment.cpp
@@ -6,45 +6,65 @@ using namespace std;
 void Investment::SetTime(int t){
 	timeMonthly = t;
 }
-int Investment::GetTime(){
-	return timeMonthly;
-}
+
 void Investment::SetAmount(double p){
 	initialAmount = p;
 }
-double Investment::GetAmount(){
-	return initialAmount;
-}
+
 void Investment::SetNumComp(int n){
 	numComp = n;
 }
-int Investment::GetNumComp() {
-	return numComp;
-}
+
 void Investment::SetInterestRate(double r){
 	intRate = r;
 }
-double Investment::GetInterestRate() {
-	return intRate;
-}
-void Investment::CalcCompoundedInt(){
+
+void Investment::CalcSimpleInt(){
 	double p = GetAmount();
 	double r = GetInterestRate();
 	int t = GetTime();
-	CompoundIntrest = (p * (1.0 + (r * t)));
+	SimpleInterest = (p * (1.0 + (r * t)));
 }
-void Investment::CalcSimpleInt(){
+
+void Investment::CalcCompoundedInt(){
 	int p = GetAmount();
 	double r = GetInterestRate();
 	int n = GetNumComp();
 	int t = GetTime();
-	SimpleIntrest = (p * (pow((1.0 + (r / n)),(n * t))));
-    
+	CompoundInterest = (p * (pow((1.0 + (r / n)),(n * t))));
 }
 
 void Investment::DisplaySimpleInt(){
+    CalcSimpleInt();
+    cout << "After: " << GetTime() << " Month(s) your new Balance is: " << GetSimpleIntrest() << endl;
     
 }
 void Investment::DisplayCompoundedInt(){
-    
+    CalcCompoundedInt();
+    cout << "After: " << GetTime() << " Month(s) your new Balance is: " << GetCompoundedInterest() << endl;
+}
+
+double Investment::GetSimpleIntrest(){
+    return SimpleInterest;
+}
+
+double Investment::GetCompoundedInterest(){
+    return CompoundInterest;
+}
+
+double Investment::GetInterestRate() {
+    return intRate;
+}
+
+double Investment::GetAmount(){
+    return initialAmount;
+}
+
+
+int Investment::GetTime(){
+    return timeMonthly;
+}
+
+int Investment::GetNumComp() {
+    return numComp;
 }
